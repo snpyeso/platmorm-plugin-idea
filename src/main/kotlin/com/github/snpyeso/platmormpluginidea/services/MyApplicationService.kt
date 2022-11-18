1db8dfa49cad017d18988bb02722318ed91d2eda
GitHub Action <action@github.com>
2022-11-18 01:13:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/snpyeso/platmormpluginidea/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/snpyeso/platmormpluginidea/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.snpyeso.platmormpluginidea.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.snpyeso.platmormpluginidea.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/snpyeso/platmormpluginidea/services/MyProjectService.kt
+++ src/main/kotlin/com/github/snpyeso/platmormpluginidea/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.snpyeso.platmormpluginidea.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.snpyeso.platmormpluginidea.MyBundle
 
 class MyProjectService(project: Project) {
 
